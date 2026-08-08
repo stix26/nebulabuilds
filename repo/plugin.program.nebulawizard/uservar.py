3d49bbd50ac76bf488720f5a6df4dc46f35a1240
stix26 <stix26@users.noreply.github.com>
2026-08-08 06:52:38 +0000

    deploy: 4d582b94653397b3ddce3849283e2dca6dab7fc8

--- repo/plugin.program.nebulawizard/uservar.py
+++ repo/plugin.program.nebulawizard/uservar.py
@@ -1,11 +1,11 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://nebulabuilds.vercel.app/builds/builds.json'
+buildfile = 'https://stix26.github.io/nebulabuilds/builds/builds.json'
 
 '''#####-----Videos File-----#####'''
 videos_url = 'http://CHANGEME'
 
 '''#####-----Notifications File-----#####'''
-notify_url  = 'http://CHANGEME'
+notify_url  = 'https://stix26.github.io/nebulabuilds/builds/notify.txt'
 
 '''#####-----Changelog Directory-----#####'''
 changelog_dir  = 'http://CHANGEME/'
