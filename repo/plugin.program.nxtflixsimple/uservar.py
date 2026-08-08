d1171ed03267548e3e79061a61facf8ba81456a0
S Seymens <sseymens@Ss-MacBook-Pro.local>
2026-08-08 03:35:27 -0500

    Initial commit

---
+++ repo/plugin.program.nxtflixsimple/uservar.py
@@ -0,0 +1,14 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://the666mafia.com/nxtflix/txt/wiz_builds.txt'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://the666mafia.com/nxtflix/txt/wizard_notify.txt'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'https://the666mafia.com/nxtflix/txt/changelog/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
